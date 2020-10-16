ef5b87c4b72b222f98cfb4561a1a86af485845d1
Jun Shimura <junshimura@gmail.com>
2020-10-16 15:33:03 +0900

    1st

---
+++ Assets/Script/Flipper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flipper : MonoBehaviour
+{
+    public float moveAngle = -45.0f;
+    public KeyCode keyCode = KeyCode.A;
+    bool isKeyDown = false;
+    float baseAngle = 0;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        baseAngle = transform.rotation.eulerAngles.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isKeyDown = Input.GetKey(keyCode);
+    }
+    private void FixedUpdate()
+    {
+        if (isKeyDown)
+        {
+            rb.MoveRotation(Quaternion.Euler(0, baseAngle + moveAngle, 0));
+        }
+        else
+        {
+            rb.MoveRotation(Quaternion.Euler(0, baseAngle, 0));
+        }
+    }
+}
